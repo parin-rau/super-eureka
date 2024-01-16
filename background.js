f8ad89d5cf5dc32fa17d188336a9055bfd0a0c9c
Parin <parin.rau@gmail.com>
2024-01-16 03:04:43 -0800

    add basic download functionality from highlighted text. remove download and open option

--- background.js
+++ background.js
@@ -3,12 +3,39 @@
 
 	// window.hasRun = true;
 
-	const downloadText = (text) => {
+	// const div = document.createElement("div");
+	// div.setAttribute("id", "clicky");
+	// document.body.appendChild(div);
+
+	// function onOpened() {
+	// 	console.log(`Opened download item`);
+	// }
+
+	// function onError(error) {
+	// 	console.log(`Error opening item: ${error}`);
+	// }
+
+	// function openDownload(downloadItems) {
+	// 	if (downloadItems.length > 0) {
+	// 		let opening = browser.downloads.open(downloadItems[0].id);
+	// 		opening.then(onOpened, onError);
+	// 	}
+	// }
+
+	// let searching = browser.downloads.search({
+	// 	limit: 1,
+	// 	orderBy: ["-startTime"],
+	// });
+
+	// searching.then(openDownload, onError);
+
+	const downloadText = async (text) => {
 		if (!text) return console.log("No text selected.");
 
 		const filename = text.split("\n")[0];
 
 		const a = document.createElement("a");
+		//a.setAttribute("target", "_blank");
 		a.setAttribute(
 			"href",
 			"data:text/plain;charset=utf-8," + encodeURIComponent(text)
@@ -23,6 +50,13 @@
 		document.body.removeChild(a);
 	};
 
+	// const getLatestDownload = () =>
+	// 	browser.downloads.search({
+	// 		limit: 1,
+	// 		orderBy: ["-startTime"],
+	// 	});
+	//.then((d) => openDownload(d[0].id));
+
 	const onCreated = () => {
 		if (browser.runtime.lastError) {
 			console.error(`Error: ${browser.runtime.lastError}`);
@@ -31,64 +65,60 @@
 		}
 	};
 
-	// function logTabs(tabs) {
-	// 	if (tabs[0].url) console.log(tabs[0].url);
-	// 	else console.log("nothing");
-	// }
-
-	// function onError(error) {
-	// 	console.error(`Error: ${error}`);
-	// }
-
-	// browser.tabs
-	// 	.query({ currentWindow: true, active: true })
-	// 	.then(logTabs, onError);
-
-	// const getCurrentTab = (tabs) => tabs[0].url;
-
 	browser.menus.create(
 		{
 			id: "save-selected",
 			title: "Save selected text",
 			contexts: ["selection"],
-			onclick: async () => {
-				await browser.tabs.sendMessage(
-					browser.tabs.query({ currentWindow: true, active: true }),
-					{ id: "save-selected" }
-				);
-			},
 		},
 		onCreated
 	);
 
 	browser.menus.create(
 		{
-			id: "always present",
-			title: "test: always present button",
-			contexts: ["all"],
-			onclick: () => {
-				console.log("clicky");
-			},
+			id: "save-selected-open",
+			title: "Save selected text and open file for editing",
+			contexts: ["selection"],
 		},
 		onCreated
 	);
 
-	browser.commands.onCommand.addListener((command) => {
-		if (command === "save-selected") {
-			console.log("save-selected");
-		}
-		if (command === "save-selected-open") {
-			console.log("save and open");
-		}
-	});
+	// browser.commands.onCommand.addListener((command) => {
+	// 	if (command === "save-selected") {
+	// 		downloadText();
+	// 	}
+	// 	if (command === "save-selected-open") {
+	// 		console.log("save and open");
+	// 	}
+	// });
 
-	browser.menus.onClicked.addListener((info, tab) => {
+	// let latestDownload = getLatestDownload()
+
+	browser.menus.onClicked.addListener(async (info, tab) => {
 		switch (info.menuItemId) {
 			case "save-selected":
 				downloadText(info.selectionText);
 				break;
+				// case "save-selected-open":
+				// downloadText(info.selectionText);
+				// latestDownload = getLatestDownload();
+				// console.log({ latestDownload });
+				// browser.downloads.open(latestDownload[0].id);
+
+				//document.body.appendChild(div);
+				// selectionText = info.selectionText;
+				// div.click();
+
+				//document.body.removeChild(div);
+
+				break;
 		}
 	});
 
-	//browser.tabs.executeScript({ file: "/content_scripts/text_saver.js" });
+	// document.querySelector("#clicky").addEventListener("click", async () => {
+	// 	const latestDownload = await downloadText(selectionText).then(
+	// 		getLatestDownload
+	// 	);
+	// 	browser.downloads.open(latestDownload[0].id);
+	// });
 })();
